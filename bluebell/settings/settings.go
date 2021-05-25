d0a49ee257f4a542eab916fbb46976b49782b614
yin-turbo <18956098339@163.com>
2021-05-25 17:56:11 +0800

    add 5.25

--- bluebell/settings/settings.go
+++ bluebell/settings/settings.go
@@ -8,10 +8,13 @@ import (
 )
 
 func Init() (err error) {
-	viper.SetConfigFile("config.yaml")
-	// viper.SetConfigName("config")
+	//方式一: 一个API就行(可以使用绝对路径或者相对路径)
+	viper.SetConfigFile("./conf/config.yaml") //这个API决定了读取的file文件的路径 与下面的AddConfigPath没有关系
+
+	//方式二: 两个API搭配使用
+	// viper.SetConfigName("config") //不需要后缀
 	// viper.SetConfigType("yaml") //专门用于从远程获取配置文件信息
-	//viper.AddConfigPath(".")    //.表示当前目录下的配置文件
+	//viper.AddConfigPath("./conf/config.yaml") //.表示当前目录下的配置文件
 	err = viper.ReadInConfig() //读取配置文件信息
 
 	if err != nil {
